9cd79e05a221c66ca1821fd06f1a190fd5722494
jameskim-glitch <enxnwk@gmail.com>
2025-05-07 16:01:21 +0900

    게시글 수정 및 이미지 업로드,삭제,수정

--- boardProject-boot/src/main/java/edu/kh/project/board/model/service/EditBoardService.java
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/service/EditBoardService.java
@@ -8,6 +8,21 @@ import edu.kh.project.board.model.dto.Board;
 
 public interface EditBoardService {
 
+	/** 게시글 작성 서비스
+	 * @param inputBoard
+	 * @param images
+	 * @return
+	 * @throws Exception
+	 */
 	int boardInsert(Board inputBoard, List<MultipartFile> images) throws Exception;
+
+	/** 게시글 업데이트 서비스
+	 * @param inputBoard
+	 * @param images
+	 * @param deleteOrderList
+	 * @return
+	 * @throws Exception
+	 */
+	int boardUpdate(Board inputBoard, List<MultipartFile> images, String deleteOrderList) throws Exception;
 
 }
--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/EditBoardMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/EditBoardMapper.java
@@ -1,6 +1,7 @@
 package edu.kh.project.myPage.model.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -21,5 +22,31 @@ public interface EditBoardMapper {
 	 * @return
 	 */
 	int insertUploadList(List<BoardImg> uploadList);
+
+	/** 게시글 수정
+	 * @param inputBoard
+	 * @return
+	 */
+	int boardUpdate(Board inputBoard);
+
+	/** 게시글 이미지 수정
+	 * @param img
+	 * @return
+	 */
+	int updateImg(BoardImg img);
+	
+	
+	/** 게시글 이미지 삭제
+	 * @param map
+	 * @return
+	 */
+	int deleteImage(Map<String, Object> map);
+
+	/** 게시글 이미지 삽입
+	 * @param img
+	 * @return
+	 */
+	int insertImg(BoardImg img);
+
 
 }
